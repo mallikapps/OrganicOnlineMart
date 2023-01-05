050242626998693570602c58e2324d12d1dd6481
mallikapps <mallik.techno@gmail.com>
2023-01-05 15:30:26 +0530

    1st update - github - organiconlinemart

---
+++ OrganicOnlineMart.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OrganicOnlineMart.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
